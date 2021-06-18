14c1f305e381c76f727c80ae85aaf0defa5fb927
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-18 14:37:02 +0530

    added: welcome message

---
+++ userregistrationdetails/UserRegistration.java
@@ -0,0 +1,14 @@
+package userregistrationdetails;
+
+import java.util.Scanner;
+
+public class UserRegistration {
+	
+	
+
+	public static void main(String[] args) {
+	
+System.out.println("User Registration System needs to ensure all validations are in during the user entry");
+	}
+
+}
