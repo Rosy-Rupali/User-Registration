114be3f35291a3f292f10e95e86681989bc58b92
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-18 14:51:18 +0530

    added: validating the first name of user

--- userregistrationdetails/UserRegistration.java
+++ userregistrationdetails/UserRegistration.java
@@ -1,14 +1,34 @@
+/**************************************************
+ * Purpose : Validating the User Details
+ * @author : Rosy Rupali
+ * @since  : 18-06-2021
+ * @version : 1.0
+ *
+ *************************************************/
 package userregistrationdetails;
 
 import java.util.Scanner;
 
 public class UserRegistration {
-	
-	
+	Scanner scanner = new Scanner(System.in);
+
+	/**
+	 * This method is validating the first name of the user is correct or not.
+	 */
+	private void validFirstName() {
+		System.out.println("Enter the first name: ");
+		String name = scanner.next();
+		String regex = "^[A-Z][a-z]{2,}";
+		if (name.matches(regex)) {
+			System.out.println("First Name of the user is valid.");
+		} else {
+			System.out.println("First Name of the user is invalid.");
+		}
+	}
 
 	public static void main(String[] args) {
-	
-System.out.println("User Registration System needs to ensure all validations are in during the user entry");
+		UserRegistration user = new UserRegistration();
+		user.validFirstName();
 	}
 
 }
